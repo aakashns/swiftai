d2353101e4d540c6265653bfe023b2bfb3149623
Aakash N S <aakashns.sky@gmail.com>
2018-06-06 22:43:17 +0530

    Update setup.py and setup.cfg

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'swiftai',
+  packages = ['swiftai'],
+  version = '0.1',
+  description = 'Utilities and helper functions for Pytorch and FastAI deep learning libraries',
+  author = 'Aakash N S',
+  author_email = 'hello@swiftace.ai',
+  url = 'https://github.com/aakashns/swiftai',
+  download_url = 'https://github.com/aakashns/swiftai/archive/v0.1.tar.gz',
+  keywords = ['deep-learning', 'pytorch', 'fastai', 'machine-learning'],
+  classifiers = [],
+)
