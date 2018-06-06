1c80d9faf65f7abf37e8521004e5b46a334cc084
Aakash N S <aakashns.sky@gmail.com>
2018-06-06 22:53:58 +0530

    Uploaded v0.1 to pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-  name = 'swiftai',
-  packages = ['swiftai'],
-  version = '0.1',
-  description = 'Utilities and helper functions for Pytorch and FastAI deep learning libraries',
-  author = 'Aakash N S',
-  author_email = 'hello@swiftace.ai',
-  url = 'https://github.com/aakashns/swiftai',
-  download_url = 'https://github.com/aakashns/swiftai/archive/v0.1.tar.gz',
-  keywords = ['deep-learning', 'pytorch', 'fastai', 'machine-learning'],
-  classifiers = [],
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="swiftai",
+  version="0.1",
+  author="Aakash N S",
+  author_email="opensource@swiftace.ai",
+  description="Utilities and helper functions for Pytorch and FastAI deep learning libraries",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/aakashns/swiftai",
+  packages=setuptools.find_packages(),
+  classifiers=(
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ),
 )
