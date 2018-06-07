893ef2a5ac50278ea8b7d98c3d13f00c697a4100
Aakash N S <aakashns.sky@gmail.com>
2018-06-07 11:14:40 -0400

    some more funcs

--- swiftai/object_detection.py
+++ swiftai/object_detection.py
@@ -1,5 +1,6 @@
 from fastai.imports import *
 from matplotlib import patches, patheffects
+from fastai.dataset import *
 
 def wh_bb(a): return np.array([a[1], a[0], a[3]+a[1]-1, a[2]+a[0]-1])
 def bb_wh(a): return np.array([a[1],a[0],a[3]-a[1]+1,a[2]-a[0]+1])
@@ -24,4 +25,12 @@ def draw_text(ax, xy, txt, sz=14):
     text = ax.text(*xy, txt, verticalalignment='top', 
                    color='white', fontsize=sz, weight='bold')
     draw_outline(text, 1)
+
+class ConcatLblDataset(Dataset):
+    def __init__(self, ds, y2): self.ds, self.y2 = ds, y2
+    def __len__(self): return len(self.ds)
+    
+    def __getitem__(self, i):
+        x, y = self.ds[i]
+        return (x, (y, self.y2[i]))
 
